e39ea041d240880282db6598ea1374d565121cc7
ahshakib <ahshakib75@gmail.com>
2024-10-30 01:19:24 +0600

    some changes in design

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -23,8 +23,8 @@ export default async function Post({ params }) {
   const post = await postPromise
   
   return (
-    <div className="p-12 bg-gradient-to-b from-slate-200 to-blue-200">
-      <div className="max-w-4xl mx-auto p-8 bg-white rounded-lg shadow-md">
+    <div className="md:p-12 sm:p-0 sm:bg-gradient-to-b from-slate-200 to-blue-200">
+      <div className="lg:max-w-4xl mx-auto p-8 bg-white rounded-lg shadow-md sm:max-w-full">
         <div className="text-center mb-8">
           <h2 className="text-3xl font-bold text-gray-900 mb-4 bg-gradient-to-r from-blue-600 to-blue-400 bg-clip-text text-transparent">
             {post.title}
